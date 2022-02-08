ed363785316c72348934076af70a2dfdb3491e4b
Eman Martin <martinemanuelleo@gmail.com>
2022-02-08 20:41:26 +0800

    adding http request to db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,7 +11,6 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
@@ -35,6 +36,7 @@ import { RecipeService } from './recipes/recipe.service';
     FormsModule,
     ReactiveFormsModule,
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [ShoppingListService, RecipeService],
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,24 +9,31 @@ import { Subject } from "rxjs";
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 
-            'This is a test description', 
-            'https://www.thespruceeats.com/thmb/yK8psUDvXdEKOvzFtLtx-n4ETuQ=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/vegan-tofu-tikka-masala-recipe-3378484-hero-01-d676687a7b0a4640a55be669cba73095.jpg',
-            [
-                new Ingredient('Pork Meat', 5),
-                new Ingredient('Potatoes', 3),
-            ]),
-        new Recipe('Hipon Sinigang', 
-            'Sinigang sa Sampalok na Hipon', 
-            'https://salu-salo.com/wp-content/uploads/2012/08/Sinigang-Hipon.jpg',
-            [
-                new Ingredient('Shrimp', 5),
-                new Ingredient('Sampalok', 3),
-            ])
-      ];
+    // private recipes: Recipe[] = [
+    //     new Recipe('A Test Recipe', 
+    //         'This is a test description', 
+    //         'https://www.thespruceeats.com/thmb/yK8psUDvXdEKOvzFtLtx-n4ETuQ=/940x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/vegan-tofu-tikka-masala-recipe-3378484-hero-01-d676687a7b0a4640a55be669cba73095.jpg',
+    //         [
+    //             new Ingredient('Pork Meat', 5),
+    //             new Ingredient('Potatoes', 3),
+    //         ]),
+    //     new Recipe('Hipon Sinigang', 
+    //         'Sinigang sa Sampalok na Hipon', 
+    //         'https://salu-salo.com/wp-content/uploads/2012/08/Sinigang-Hipon.jpg',
+    //         [
+    //             new Ingredient('Shrimp', 5),
+    //             new Ingredient('Sampalok', 3),
+    //         ])
+    //   ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private slService: ShoppingListService) { }
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
+    }
 
     getRecipes() {
         return this.recipes.slice();
