33de51a28897b7141ce7a03a49bb520148b758a6
Eman Martin <martinemanuelleo@gmail.com>
2022-02-11 20:12:49 +0800

    new changes

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { AuthService } from "./auth.service";
 
 @Component({
     selector: 'app-auth',
@@ -8,7 +10,31 @@ import { Component } from "@angular/core";
 export class AuthComponent {
     isLoginMode = true;
 
+    constructor(private authService: AuthService) {}
+
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
+    }
+
+    onSubmit(form: NgForm){
+        if (!form.valid) {
+            return;
+        }
+        const email = form.value.email;
+        const password = form.value.password;
+
+        if(this.isLoginMode){
+            //...
+        } else {
+            this.authService.signup(email, password)
+            .subscribe(
+                resData => {
+                    console.log(resData);
+                }, error => {
+                    console.log(error);
+                }
+            );
+        }
+        form.reset();
     }
 }
